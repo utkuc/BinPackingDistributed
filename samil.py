73587c4353bd59596efea5998ac588ca63abc03d
Utku Cuhadaroglu <utku.cuhadaroglu@gmail.com>
2021-04-19 23:29:08 +0300

    Simple example of parallel sum operation. Implementation and map explanation for future.

--- samil.py
+++ samil.py
@@ -1,36 +1,77 @@
-import json
-import csv
-import os
 import random
-
-import pandas as pd
 from pyspark.sql import SparkSession
-from pyspark import SparkConf, SparkContext
-from pyspark.sql.types import StructField, StructType, StringType, IntegerType
+from pyspark.sql.types import StructType, StructField, StringType, IntegerType
+from pyspark.sql import Row
 
-spark = SparkSession.builder.appName("ddds").master("spark://192.168.0.10:7077").getOrCreate()
-columns = ["valueid", "value", "currentbinid"]
+spark = SparkSession.builder.appName("ddds").master("spark://172.20.64.1:7077").getOrCreate()
+data_schema = StructType([
+    StructField("ValueId", IntegerType(), False),
+    StructField("Value", IntegerType(), True),
+    StructField("BinId", IntegerType(), True)
+
+])
+bin_schema = StructType([
+    StructField("BinId", IntegerType(), False),
+    StructField("BinUtilization", IntegerType(), False),
+    StructField("BinCapacity", IntegerType(), False)
+
+])
+bin_capacity = 130
 data = []
-
-#Create Items with value between (0-100) for packing
-for i in range(0, 1000):
-    value = (i, random.randint(0, 100), 0)
+bins = []
+# Create 1000 Items with value between (0-100) for packing
+for i in range(0, 10):
+    value = (i, random.randint(0, 100), None)
     data.append(value)
-rdd = spark.sparkContext.parallelize(data,4)
-dfFromRDD1 = rdd.toDF(columns)
+data_df = spark.createDataFrame(data=data, schema=data_schema)
+bin_df = spark.createDataFrame(data=bins, schema=bin_schema)
 
-def testFunction(row):
-    valueid=row.valueid
-    value=100+row.value
-    currentbinid = row.currentbinid
-    return valueid, value, currentbinid
-
-rdd2 = dfFromRDD1.rdd.map(lambda x: testFunction(x))
-print(rdd2.take(100))
+# To print each nodes data:
+# print(data_df.rdd.glom().collect())
+flag = True
 
 
+# How to sum values for each partition at each node: This is parallel operation
+# spark.filter -> BinId = null ise kutuya konmaya ihtiyacı var.
+# bin_df.where  ile şu andaki utilizationın altında kalan binlere bak:
+# bu binlerden: BinCapacity ve BinUtilization dan şu andaki kapasiteyi çıkar.
+# Eğer kapasite şu anda işlenen 'value' için yeterliyse, binid burdaki bine eşlenir.
+# Bunların hepsini yaparken yeni RDD ler yaratmak zorundayız, dataframe ve rdd immutable veri yapıları.
+def show(index, iterator):
+    sum = 0
+    for row in iterator:
+        # Burda print Sum dersen: pycharm print atmaz ama workerlar kendi outputlarında gösterir, örnek:
+        # print(row.Value) bunu sadece workerin kendi outputunda görebilirsin. Mastera collect atmadıgın sürece
+        # aşağıda yield dan sonra collect atılcak, yani sumlar workerlarda hesaplanıp sadece sum sonuçları masterda birleşcek/gösterilcek.
+        sum += row.Value
+    yield 'Partition Index: ' + str(index) + " Sum of data values in partition: " + str(sum)
 
-dfFromRDD1.show(truncate=False)
+
+# TODO: Runs for 1 time. We need to determine when its enough.
+while (flag):
+    # If there are no bins, create a new bin
+    if len(bin_df.take(1)) == 0:
+        print("There are no bins, creating new bin...")
+        new_bin = [[0, 0, bin_capacity]]
+        new_bin_df = spark.createDataFrame(new_bin)
+        bin_df = bin_df.union(new_bin_df)
+
+    print(data_df.rdd.mapPartitionsWithIndex(show).collect())
+    flag = False
+bin_df.show(truncate=False)
+#
+# def testFunction(row):
+#     valueid=row.valueid
+#     value=100+row.value
+#     currentbinid = row.currentbinid
+#     return valueid, value, currentbinid
+#
+# rdd2 = dfFromRDD1.rdd.map(lambda x: testFunction(x))
+# print(rdd2.take(100))
+#
+#
+#
+# dfFromRDD1.show(truncate=False)
 
 
 # Utilization Perc: %90
